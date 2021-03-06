ecc29bbb2cc38859b78624b9346f2a452f3cc311
Peter DeVita <mewtwo2643@yahoo.com>
2021-03-06 18:06:17 -0500

    Add files for unit tests

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import praw
+import re
+from praw.models import Comment, Submission
 import datetime as dt
 from psaw import PushshiftAPI
 from core.credentials import CredentialsLoader
@@ -8,16 +10,29 @@ credentials = CredentialsLoader.get_credentials()['reddit']
 
 reddit = praw.Reddit(client_id=credentials["client_id"],
                      client_secret=credentials["client_secret"],
-                     user_agent=consts.user_agent.format(consts.version),
+                     user_agent=consts.user_agent,
                      username=credentials["username"],
                      password=credentials["password"])
 
 api = PushshiftAPI(reddit)
 
-start = int(dt.datetime(2017, 8, 18).timestamp())
-end = int(dt.datetime(2017, 8, 21).timestamp())
+start = int(dt.datetime(2019, 9, 2).timestamp())
+end = int(dt.datetime(2019, 9, 4).timestamp())
 
-thing = api.search_submissions(after=start, before=end, subreddit='switcharoo')
+# thing = api.search_submissions(after=start, before=end, subreddit='switcharoo')
+
+thing = api.search_comments(subreddit='gifs', after=start, before=end,)
+
+grb = re.compile('gifreversingbot', flags=re.I)
 
 for i in thing:
-    print(i, i.title, i.author, i.url)
+    if isinstance(i, Submission):
+        print(i, i.title, i.author, i.url)
+    elif isinstance(i, Comment):
+        matches = grb.findall(i.body)
+        if matches and i.author.name != 'GifReversingBot':
+            print(i, i.author, dt.datetime.utcfromtimestamp(i.created_utc).replace(tzinfo=dt.timezone.utc).astimezone(), "https://reddit.com"+i.permalink)
+
+
+
+
