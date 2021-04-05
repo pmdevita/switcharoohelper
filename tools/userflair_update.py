cf74f1c4086370678ce45ebd5eb29abed7f43fd1
Peter DeVita <mewtwo2643@yahoo.com>
2021-04-05 18:06:54 -0400

    Fix flair updates for deleted users polluting moderation log

--- tools/userflair_update.py
+++ tools/userflair_update.py
@@ -11,6 +11,7 @@ from core.credentials import CredentialsLoader
 credentials = CredentialsLoader.get_credentials(tools_folder / "../credentials.ini")['reddit']
 
 import praw
+import prawcore.exceptions
 from core.history import SwitcharooLog
 from core import constants as consts
 
@@ -28,6 +29,21 @@ last_switcharoo = SwitcharooLog(reddit)
 def pad_zeros(number, zeros):
     string = str(number)
     return "".join(["0" for i in range(max(zeros - len(string), 0))]) + string
+
+
+def remove_deleted_users(flairs):
+    delete_list = []
+    # Double check that the users exist before committing changes
+    for i, flair in enumerate(flairs):
+        try:
+            redditor = reddit.redditor(flair['user'])
+            status = redditor.is_suspended
+        except prawcore.exceptions.NotFound:
+            print(flair['user'], "no longer exists, omitting")
+            delete_list.append(i)
+    for i in reversed(delete_list):
+        flairs.pop(i)
+    return flairs
 
 
 print("SwitcharooHelper Flair Sync v{} Ctrl+C to stop".format(consts.version))
@@ -49,7 +65,7 @@ if None in current_good:
 final_flairs = {key: f"badge-{pad_zeros(current_good[key][0], 2)}" for key in current_good}
 delete_list = []
 
-# Omit anyone with a troublemaker flair
+# Omit anyone with a troublemaker flair or already has the flair
 for flair in switcharoo.flair():
     if flair['flair_css_class']:
         if flair['flair_css_class'][:6] != "badge-":
@@ -73,6 +89,6 @@ final_flairs += [{"user": user, "flair_css_class": "", "flair_text": ""} for use
 
 print("Updating flairs...")
 
-switcharoo.flair.update(final_flairs)
+switcharoo.flair.update(remove_deleted_users(final_flairs))
 
 print("Done!")
